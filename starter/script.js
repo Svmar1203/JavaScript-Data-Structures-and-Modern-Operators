610cd4e15261a94f572a04642b197cc059fac2aa
Svmar1203 <83650741+Svmar1203@users.noreply.github.com>
2021-11-05 23:01:45 +0200

    Add files via upload

--- starter/script.js
+++ starter/script.js
@@ -43,3 +43,90 @@ orderSet.add('Garlic Bread');
 //console.log(new Set('Svetlana'));
 
 for (const order of orderSet) console.log(order);
+
+const staff = ['Waiter', 'Chef', 'Waiter', 'Chef', 'Manager', 'Waiter'];
+const stafNew = new Set(staff);
+console.log(stafNew);
+const stafSecond = [...new Set(staff)];
+console.log(stafSecond);
+console.log(new Set('svetlanasvetasv').size);
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+rest.set(2, 'Lisbon, Portugal');
+console.log(rest);
+
+//set method return updated array
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+console.log(rest);
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(false));
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(rest.has('categories'));
+rest.delete(2);
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest.get(arr));
+console.log(rest);
+console.log(rest.size);
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+//rest.clear();
+//console.log(rest);
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try again'],
+]);
+
+console.log(question);
+console.log(Object.entries(restaurant.openingHours));
+const restMap = new Map(Object.entries(restaurant.openingHours));
+console.log(restMap);
+
+//Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+const answer = Number(prompt('Your answer: '));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+/////--Convert map to array--//////////
+console.log([...question]);
+console.log(question.entries());
+console.log([...question.keys()]);
+console.log(question.values());
+
+const gameEvents = new Map([
+  [17, 'GOAL'],
+  [36, 'Substitution'],
+  [47, 'GOAL'],
+  [61, 'Substitution'],
+  [64, 'Yellow card'],
+  [69, 'Red card'],
+  [70, 'Substitution'],
+  [72, 'Substitution'],
+  [76, 'GOAL'],
+  [80, 'GOAL'],
+  [92, 'Yellow card'],
+]);
