a3808bda62b1e20f01cf45da7756042c56c91727
Svmar1203 <83650741+Svmar1203@users.noreply.github.com>
2021-11-07 23:26:59 +0200

    Add files via upload

--- starter/script.js
+++ starter/script.js
@@ -150,11 +150,20 @@ console.log(average);
 console.log(`An event happened, on average, every ${average} minutes`);
 const timee = [...gameEvents.keys()];
 console.log(timee);
+console.log(timee.length);
+console.log(timee.pop());
 
+/*
 for (const [keyy, valuee] of gameEvents) {
   if (
     keyy <= 45
       ? console.log(`[FIRST HALF] ${keyy}: ${valuee}`)
       : console.log(`[second half] ${keyy}: ${valuee}`)
   );
+}
+*/
+
+for (const [min, eventss] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF]: ${min}: ${eventss}`);
 }
