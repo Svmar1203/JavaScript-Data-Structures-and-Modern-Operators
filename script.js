9df63e3eee5e0adf0c97edd861dfb484a473fd4f
Svmar1203 <83650741+Svmar1203@users.noreply.github.com>
2021-11-12 23:48:23 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,15 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const getCode = str => str.slice(0, 3).toUpperCase();
+
 for (const flight of flights.split('+')) {
   const [type, from, to, time] = flight.split(';');
-  console.log(type, from, to, time);
-  const output = `${type} ${from} ${to} (${time.replace(':', 'h')})`;
+
+  const output = `${type.startsWith('_Delayed') ? '❌' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} (${time.replace(':', 'h')})`;
   console.log(output);
 }
 // Data needed for first part of the section
