60913f9f8ea189ddf2d2da09dff4a465835ba4ff
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2021-11-13 14:02:44 +0200

    added new folder

---
+++ 10-Functions/starter/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassenger = 1, price = 199) {
+  // ES5 numPassenger = numPassenger || 1;
+  //ES5 price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
