0a612a7f1d9fad3ddbdb360bed8dbb94a4e54f7d
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2021-11-15 22:00:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -227,12 +227,30 @@ console.log(addVAT2(200));
 const poll = {
   question: 'What is your favorite programming language?',
   options: ['0: JAvaScript', '1: Python', '2: Rust', '3: C++'],
-  answers: newArray(4).fill(0),
+  answers: new Array(4).fill(0),
   registerNewAnswer() {
     const answer = Number(
       prompt(
         `${this.question}\n${this.options.join('\n')}\n(Write option number)`
       )
     );
+    console.log(answer);
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type == 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
   },
 };
+
+poll.registerNewAnswer();
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
