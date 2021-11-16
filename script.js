ad226c0aa19a6d3e3ac3803711b5361fb478c9c1
Svetlana <83650741+Svmar1203@users.noreply.github.com>
2021-11-16 23:06:22 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -280,9 +280,51 @@ const secureBooking = function () {
 
   return function () {
     passengerCount++;
-    console.log(`${passengerCount} passengers`);
+    console.log(passengerCount);
   };
 };
 
 const booker = secureBooking();
 booker();
+booker();
+booker();
+
+////--Example 1--/////////////
+/*
+let f;
+
+const g = function () {
+  const z = 23;
+
+  f = function () {
+    console.log(z * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+h();
+f();
+console.dir(f);
+*/
+
+////////////////--Example 2--////////////////
+const boardPAssengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPAssengers(180, 3);
